92d0be340caab4ad584f1d218c9e2af8a3d44412
Ori Clawd <OriClawd@proton.me>
2026-03-03 12:22:01 -0800

    Fix basePath for GitHub Pages

--- ua-conneckit/next.config.mjs
+++ ua-conneckit/next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/universal-wallet-web',
+  assetPrefix: '/universal-wallet-web/',
   images: {
     unoptimized: true,
   },
